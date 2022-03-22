d63bf558f7b8967c703276a860715ea9da273ae6
Yaqian Yang <yang.yaqi@northeastern.edu>
2022-03-21 20:45:34 -0700

    a6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import './vendors/bootstrap/css/bootstrap.min.css';
+import './vendors/bootstrap/bootstrap.min.css';
+import './vendors/fontawesome/css/all.min.css';
+import HelloWorld from "./components/HelloWorld";
+import Labs from "./components/Labs";
+import Tuiter from "./components/Tuiter";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
-function App() {
+function App()  {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="container">
+        <Routes>
+            <Route path="/hello" element={<HelloWorld/>}/>
+            <Route path="/labs" element={<Labs/>}/>
+            <Route path="/tuiter" element={<Tuiter/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
-
 export default App;
+//function App() {
+//  return (
+//    <div className="App">
+//      <header className="App-header">
+//        <img src={logo} className="App-logo" alt="logo" />
+//        <p>
+//          Edit <code>src/App.js</code> and save to reload.
+//        </p>
+//        <a
+//          className="App-link"
+//          href="https://reactjs.org"
+//          target="_blank"
+//          rel="noopener noreferrer"
+//        >
+//          Learn React
+//        </a>
+//      </header>
+//    </div>
+//  );
+//}
+//
+//export default App;
---
+++ src/components/Labs.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HelloWorld = () => {
+    return(
+        <h1>Labs.js</h1>
+    )
+};
+
+export default HelloWorld;
